77917e91260fea6b5686f20a788f204ee384bab0
Gaston Gomez <gaston.miguel.gomez@gmail.com>
2023-07-05 02:41:59 -0300

    terminando estructura html

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const PORT = 3030;
+
+app.use(express.static(path.join(__dirname,"public")))
+
+app.listen(PORT,() => console.log(`Server is runing in port http://localhost:${PORT}`))
+
+app.get("/",(req,res) => res.sendFile(path.join(__dirname,"views","home.html")))
+
+
