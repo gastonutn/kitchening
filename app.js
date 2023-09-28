a1eb1171a3cf0cf19ba5b348f15ac0f3167d9ad0
Gaston Gomez <gaston.miguel.gomez@gmail.com>
2023-09-28 10:51:32 -0300

    fix validations

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const methodOverride = require('method-override');
 const PORT = 3030;
 
-app.use(express.static(path.join(__dirname,"public")))
+
+app.use(express.static(path.join(__dirname,"public")));
+
+app.use(methodOverride('_method'));
 
 app.listen(PORT,() => console.log(`Server is runing in port http://localhost:${PORT}`))
 
